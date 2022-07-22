9dc17751a15ba00b6db8a2beee58d9d437f3774e
alessandracostabc <alessandracostabc@gmail.com>
2022-07-22 13:56:26 -0300

    alteração cod list service

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,7 +4,18 @@ const controller = {
     index: (request, response) => {
       const listUser = ListUserService.listUserService()
       response.json(listUser)
-    }
+    },
+    ListData: (request, response) => {
+        const { name } = request.query;
+    
+        if (!name) {
+          return response.status(400).json({ "erro": "Você não informou um nome" })
+        }
+    
+        const legendary = ListUserService.listUserData(name);
+    
+        return response.json(user)
+      }
    
     }
 
--- src/services/user/list.js
+++ src/services/user/list.js
@@ -1,39 +1,10 @@
 const fs = require('fs')
+
 const path =  'src/app/service/user/dbUser.json'
 const user = JSON.parse(fs.readFileSync(path, 'utf-8'))
 
 const ListUserService = {
     listUser: () => {
-      const user = new User(
-        1,
-        id,
-        'name',
-        'password',
-        'birthData',
-        'cpf',
-        'telephone',
-        'address',
-        'email',
-        'adm'
-      )
-      const user2 = new User(
-        2,
-        id, 
-        'name',
-        'password',
-        'birthData',
-        'cpf',
-        'telephone',
-        'address',
-        'email',
-        'adm'
-      )
-  
-      return [user, user2]
-    },
-    listUserData: (UserName) => {
-      const userList = ListUserService.listUserService();
-      const user = userList.find(item => item.name === userName);
       return user
     }
   }
