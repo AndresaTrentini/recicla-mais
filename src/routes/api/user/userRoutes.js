7605ef1755c8cbd18cf3c474d41e475469d5a759
Robson Rosa <contatorobsonrosa@gmail.com>
2022-07-26 16:10:54 -0300

    adicionado middleware de administrador para as rotas necessárias

--- src/routes/api/user/userRoutes.js
+++ src/routes/api/user/userRoutes.js
@@ -3,19 +3,21 @@ const userController = require("../../../controllers/user")
 const validationUser = require("./validation")
 const { signin, validateToken } = require('../../../controllers/auth')
 const passport = require('../../../config/passport')
+const admin = require('../../../middlewares/admin')
 
 const app = express()
 
 app.route('/users')
     .all(passport())
-    .post(validationUser, userController.create)
-    .put(validationUser, userController.update)
-    .get(userController.list)
-    .delete(userController.delete)
+    .post(admin, validationUser, userController.create)    
+    .get(admin, userController.list)
+    
 
 app.route("/users/:id")
     .all(passport())
-    .get(userController.ListById)
+    .get(admin, userController.ListById)
+    .delete(admin, userController.delete)
+    .put(admin, validationUser, userController.update)
 
 app.post('/signup', userController.create)
 app.post('/signin', signin)
