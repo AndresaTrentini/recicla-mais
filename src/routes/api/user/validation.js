93e28c0cd8db8de7f059fa5389cce288978915d0
Robson Rosa <contatorobsonrosa@gmail.com>
2022-07-26 11:07:02 -0300

    update user Validation. Fixed bug.

--- src/routes/api/user/validation.js
+++ src/routes/api/user/validation.js
@@ -1,25 +1,29 @@
 const yup = require('yup');
 
-async function userValidator(req, res, next){
-    let schema = yup.object().shape({
-        name: yup.string().strict().required(),
-        password: yup.string().required(),
-        birthData: yup.date().required(),
-        cpf: yup.number().required(),
-        telephone: yup.string().required(),
-        address: yup.string().required(),
-        email: yup.string().email(),
-        adm: yup.boolean().default(false)
-        
-      });
-    
-      await schema.validate(req.body, { abortEarly: false }).catch(err => {
-        return res.status(400).json({
-            error: err.errors
-        })
-      })
-      next();
+async function userValidator(req, res, next) {
+  let schema = yup.object().shape({
+    name: yup.string().strict().required("Nome é obrigatório"),
+    password: yup.string().required("Senha é obrigatório"),
+    confirmPassword: yup.string().required("Confirme sua senha").oneOf([yup.ref('password')], "Senhas diferentes!"),
+    birthData: yup.date().required("Data de Nascimento é obrigatório"),
+    cpf: yup.number().required("CPF é obrigatório"),
+    telephone: yup.string().strict("Telefone é obrigatório").required(),
+    address: yup.string().strict().required("Endereço é obrigatório"),
+    email: yup.string().strict().email().required("Email é obrigatório"),
+    adm: yup.boolean().default(false)
 
+  });
+
+  try {
+    await schema.validate(req.body, { abortEarly: false })
+    return next()
+
+  } catch (err) {
+    return res.status(400).json({
+      message: err.errors,
+      data: err.inner
+    })
+  }
 }
 
 module.exports = userValidator;
