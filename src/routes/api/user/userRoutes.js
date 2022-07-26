da813423a7a29eab0cd555b8a56cb9423707a128
Robson Rosa <contatorobsonrosa@gmail.com>
2022-07-26 10:02:50 -0300

    organizado rotas de users

--- src/routes/api/user/userRoutes.js
+++ src/routes/api/user/userRoutes.js
@@ -1,8 +1,16 @@
-const Router = require("express")
+const express = require("express")
 const userController = require("../../../controllers/user")
 const validationUser = require("./validation")
 
-const routes = new Router()
-routes.post("/users", validationUser, userController.create)
+const app = express()
 
-module.exports = routes
+app.route('/users')
+    .post(validationUser, userController.create)
+    .put(validationUser, userController.update)
+    .get(userController.index)
+    .delete(userController.delete)
+
+app.route("/users/:id")
+    .get(userController.ListById)
+
+module.exports = app
