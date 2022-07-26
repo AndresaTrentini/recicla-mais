128b7bf2ff2a870d7cb2bd0f699e3126e30cf15c
Robson Rosa <contatorobsonrosa@gmail.com>
2022-07-26 13:54:37 -0300

    adicionado middleware nas rotas para verificação de autencicação de usuário

--- src/routes/api/user/userRoutes.js
+++ src/routes/api/user/userRoutes.js
@@ -1,16 +1,24 @@
 const express = require("express")
 const userController = require("../../../controllers/user")
 const validationUser = require("./validation")
+const { signin, validateToken } = require('../../../controllers/auth')
+const passport = require('../../../config/passport')
 
 const app = express()
 
 app.route('/users')
+    .all(passport())
     .post(validationUser, userController.create)
     .put(validationUser, userController.update)
     .get(userController.list)
     .delete(userController.delete)
 
 app.route("/users/:id")
+    .all(passport())
     .get(userController.ListById)
+
+app.post('/signup', userController.create)
+app.post('/signin', signin)
+app.post('/validate-token', validateToken)
 
 module.exports = app
