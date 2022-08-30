018fb7e38f42392656854534ef27b0c1381b49ff
Robson Rosa <contatorobsonrosa@gmail.com>
2022-08-30 19:51:10 -0300

    refatorado auth e create user

---
+++ src/controllers/AuthUserController.js
@@ -0,0 +1,34 @@
+import UserAuthService from '../services/user/AuthUserService'
+
+class AuthUserController {
+    constructor() {
+        this.service = new UserAuthService()
+    }
+
+    async signin(request, response) {
+
+        const { email, password } = request.body
+
+        if (!email || !password) {
+            return response.status(400).send('Informe usuário e senha!')
+        }
+
+        const user = await this.service.authService(email, password)
+
+        response.status(user.status).json(user.data)
+
+    }
+
+    validateToken(request, response) {
+        const user = request.body || null
+        const validate = this.service.validateTokenService(user)
+
+        return validate
+    }
+}
+
+const authUserController = new AuthUserController()
+const signin = authUserController.signin
+const validateToken = authUserController.validateToken
+
+export default { signin, validateToken } 
--- src/controllers/user/CreateUserController.js
+++ src/controllers/user/CreateUserController.js
@@ -1,16 +1,16 @@
-import CreateUserService from "../../services/user/create";
+import CreateUserService from "../../services/user/CreateUserService";
 
 class CreateUserController {
     constructor() { }
 
-    static create (request, response) {
+    static async create (request, response) {
         const { name, password, birthData, cpf, telephone, address, email} = request.body
         let { adm } = request.body
     
         if (!request.originalUrl.startsWith('/users')) adm = false
         if (!request.user || !request.user.adm) adm = false
     
-        const user = CreateUserService(name, birthData, cpf, telephone, email, address, password, adm)
+        const user = await CreateUserService.create(name, birthData, cpf, telephone, email, address, password, adm)
     
         response.status(user.status).json(user)
       }
