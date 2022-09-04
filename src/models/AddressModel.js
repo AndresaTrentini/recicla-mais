7f53d1e7bba3f9635812b5ab40c7b2d47443f4d0
tldcastro <talitalimadc@gmail.com>
2022-09-04 20:20:32 -0300

    Subindo o AddressModel

---
+++ src/models/AddressModel.js
@@ -0,0 +1,34 @@
+import Sequelize, { Model } from "sequelize";
+import databaseConfig from '../config/db'
+const sequelize = new Sequelize(databaseConfig)
+export default class AdressModel extends Model { }
+
+AddressModel.init(
+    {
+        id: {
+            type: Sequelize.UUIDV4(),
+            defaultValue: Sequelize.UUIDV4(),
+            primaryKey: true
+        },
+        
+        street: {type: Sequelize.STRING, 
+            allowNull:false},
+        district: {type: Sequelize.STRING, 
+            allowNull:false},
+        city: {type: Sequelize.STRING, 
+            allowNull:false},
+        state: {type: Sequelize.STRING(2), 
+                allowNull:false},
+        country: {type: Sequelize.STRING, 
+            allowNull:false},
+        zip: {type: Sequelize.STRING(8), 
+            allowNull:false},
+        number: {type: Sequelize.INTEGER, 
+            allowNull:false},
+    },
+    {
+        sequelize,
+        modelName: 'address',
+        paranoid: true
+    }
+);
