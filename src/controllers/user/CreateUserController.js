3ec4154b1cc685e70a52c964b244ef4aea8cf881
Robson Rosa <contatorobsonrosa@gmail.com>
2022-09-05 21:38:50 -0300

    ajustes no User Controller

--- src/controllers/user/CreateUserController.js
+++ src/controllers/user/CreateUserController.js
@@ -4,13 +4,14 @@ class CreateUserController {
     constructor() { }
 
     static async create (request, response) {
-        const { name, password, birthData, cpf, telephone, address, email} = request.body
+        const { name, password, birthData, cpf, telephone, email} = request.body
         let { adm } = request.body
-    
-        if (!request.originalUrl.startsWith('/users')) adm = false
-        if (!request.user || !request.user.adm) adm = false
-    
-        const user = await CreateUserService.create(name, birthData, cpf, telephone, email, address, password, adm)
+
+        if (!request.originalUrl.startsWith('/users')) adm = 0
+        if (!request.user || !request.user.adm) adm = 0
+
+        const service = new CreateUserService()
+        const user = await service.create(name, birthData, cpf, telephone, email, password, adm)
     
         response.status(user.status).json(user)
       }
