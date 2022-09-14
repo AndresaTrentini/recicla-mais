a29a2e4e355506a660f58bce04c3c6bdd20222dc
Robson Rosa <contatorobsonrosa@gmail.com>
2022-09-14 18:34:46 -0300

    update routes

--- src/routes/api/scheduling/schedulingRoutes.js
+++ src/routes/api/scheduling/schedulingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import passport from '../../../config/passport'
+import passport from '../../../middlewares/passport'
 
 import ListSchedulingController from "../../../controllers/scheduling/ListSchedulingConstroller"
 import DaysAvailableSchedulingController from "../../../controllers/scheduling/DaysAvailableSchedulingController"
@@ -7,16 +7,15 @@ import CreateSchedulingController from "../../../controllers/scheduling/CreateSc
 
 const app = express()
 
+app.use(passport)
+
 app.route('/scheduling')
-	.all(passport)
-	.post((req, res) => CreateSchedulingController.create(req, res) )
+	.post((req, res) => CreateSchedulingController.create(req, res))
 
 app.route('/scheduling/filter')
-	.all(passport)
 	.get((req, res) => ListSchedulingController.list(req, res))
 
 app.route('/scheduling/days-available')
-	.all(passport)
 	.get((req, res) => DaysAvailableSchedulingController.list(req, res))
 
 export default app
--- src/routes/api/user/userRoutes.js
+++ src/routes/api/user/userRoutes.js
@@ -3,10 +3,11 @@ import CreateUserController from "../../../controllers/user/CreateUserController
 import DeleteUserController from "../../../controllers/user/DeleteUserController"
 import ListUserController from "../../../controllers/user/ListUsersController"
 import UpdateUserController from "../../../controllers/user/UpdateUserController"
-import validationUser from "./validation"
+import validationUser from "../../../middlewares/validationCreateUser"
 import AuthUserController from '../../../controllers/user/AuthUserController'
-import passport from '../../../config/passport'
+import passport from '../../../middlewares/passport'
 import admin from '../../../middlewares/admin'
+import validationUpdateUser from '../../../middlewares/validationUpdateUser'
 import DeleteUserController from "../../../controllers/user/DeleteUserController"
 
 const app = new express()
@@ -14,29 +15,30 @@ const app = new express()
 const deleteUserController = new DeleteUserController()
 const listUserController = new ListUserController()
 const updateUserController = new UpdateUserController()
-
-app.route('/users')
-    .all(passport)
-    .post(admin, validationUser, (req, res) => CreateUserController.create(req, res))
-    .get(admin, (req, res) => listUserController.list(req, res))
-
-app.route('/profile')
-    .all(passport())
-    .get( (req, res) => listUserController.getProfile(req, res))
-    .delete( (req, res) => deleteUserController.delete(req, res))
-    .put( validationUser, (req, res) => updateUserController.update(req, res))
-
-
-app.route("/users/:id")
-    .all(passport)
-    .get(admin, (req, res) => listUserController.listbyid(req, res))
-    .delete(admin, (req, res) => deleteUserController.delete(req, res))
-    .put(admin, validationUser, (req, res) => updateUserController.update(req, res))
-
 const authUserController = new AuthUserController()
 
 app.post('/signup', validationUser, (req, res) => CreateUserController.create(req, res))
 app.post('/signin', (req, res) => authUserController.signin(req, res))
-app.post('/validate-token', (req, res) => authUserController.validateToken(req, res))
+
+app.use(passport)
+
+app.route('/users')
+    .all(admin)
+    .post(validationUser, (req, res) => CreateUserController.create(req, res))
+    .get((req, res) => listUserController.list(req, res))
+
+app.route("/users/:id")
+    .all(admin)
+    .get((req, res) => listUserController.listbyid(req, res))
+    .delete((req, res) => deleteUserController.delete(req, res))
+    .put(validationUpdateUser, (req, res) => updateUserController.update(req, res))
+
+app.route('/profile')
+    .get( (req, res) => listUserController.getProfile(req, res))
+    .delete( (req, res) => deleteUserController.delete(req, res))
+    .put( validationUpdateUser, (req, res) => updateUserController.update(req, res))
+
+app.route('/profile/password')
+    .patch((req, res) => updateUserController.password(req, res))
 
 export default app
