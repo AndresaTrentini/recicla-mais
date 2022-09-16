1238b0e8925ba7627ce95584fe4e39a9da190ea6
Robson Rosa <contatorobsonrosa@gmail.com>
2022-09-16 17:13:35 -0300

    preparando projeto para deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app'
 import 'dotenv/config'
 
-app.listen(process.env.API_PORT, () => {
+app.listen(process.env.PORT || process.env.API_PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.API_PORT}`)
 })
