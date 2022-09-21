7f1faa9ffdcf2730ba3f541384d68b53e61ba7b6
Robson Rosa <contatorobsonrosa@gmail.com>
2022-09-21 18:00:55 -0300

    update css

--- src/routes/api/swaggerDocumentation.js
+++ src/routes/api/swaggerDocumentation.js
@@ -6,8 +6,9 @@ import swaggerDoc from "../../../swagger.json"
 const app = new express()
 
 var options = {
-    customCss: '.swagger-ui .topbar { display: none }',
+    customCss: '.swagger-ui .topbar { display: none } section.models { display: none }',
     customSiteTitle: "Recicla Mais API",
+    
   };
 
 app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc, options))
