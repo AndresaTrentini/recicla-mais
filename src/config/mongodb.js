a70abb6a7fb93054352372dbfe078b90595e0fa9
Robson Rosa <contatorobsonrosa@gmail.com>
2022-09-23 20:26:17 -0300

    update connect mongo

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.16ci17i.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`, { useNewUrlParser: true })
 .catch(err => {
     const msg = "ERRO! Não foi possível conectar com o MongoDB!"
     console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
