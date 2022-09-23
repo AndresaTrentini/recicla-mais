8025dadbe9db0fca72e984a171ab81fa8bbe2205
Robson Rosa <contatorobsonrosa@gmail.com>
2022-09-23 20:34:01 -0300

    update import connect mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app'
 import 'dotenv/config'
-
+import "./config/mongodb"
 app.listen(process.env.PORT || process.env.API_PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.API_PORT}`)
 })
